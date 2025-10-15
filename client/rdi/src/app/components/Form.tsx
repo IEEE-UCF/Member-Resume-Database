9a884734587ee90c57fcd04c2497351306f740a4
Tal Avital <tal.avital04@gmail.com>
2025-10-15 15:09:18 -0400

    start back end

--- client/rdi/src/app/components/Form.tsx
+++ client/rdi/src/app/components/Form.tsx
@@ -1,7 +1,15 @@
 const Form = () => {
+    const handleSubmit = () => {
+        console.log("HI")
+    }
+
     return (
         <>
-            
+            <form onSubmit={() => handleSubmit}>
+                <button
+                    type = "submit"
+                />
+            </form>
         </>
     )
 }
