af36a6106bb264571d2fd9e7ac218e2fe1533ec9
Tal Avital <tal.avital04@gmail.com>
2025-10-30 17:13:59 -0400

    add links component to form

--- form/src/app/components/Form.tsx
+++ form/src/app/components/Form.tsx
@@ -282,37 +282,9 @@ const Form = () => {
                 </div>
 
                 <div className="input links">
-                    <h3>Links</h3>
-                    {formData.links.map((link, index) => {
-                        return (
-                            <input
-                                key={`links[${index}]`}
-                                type="url"
-                                name={`links[${index}]`}
-                                placeholder="https://example.com"
-                                value={link}
-                                onChange={(e) =>
-                                    setFormData((prev) => {
-                                        const result = [...prev.links];
-                                        result[index] = e.target.value;
-                                        return { ...prev, links: result };
-                                    })
-                                }
-                            />
-                        );
-                    })}
-                    <input
-                        type="button"
-                        value="Add Link"
-                        onClick={(e) => {
-                            e.preventDefault()
-                            return (
-                                setFormData((prev) => ({
-                                    ...prev,
-                                    links: [...prev.links, ""],
-                                }))
-                            )
-                        }}
+                    <LinksComponent
+                        links = {formData.links}
+                        setFormData = {setFormData} 
                     />
                 </div>
 
