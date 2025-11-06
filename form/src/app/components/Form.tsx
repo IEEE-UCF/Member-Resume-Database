1ea4b0979b67b2213d389f21ff2fc358af337b46
Tal <tal.avital04@gmail.com>
2025-11-06 16:38:42 -0500

    working sites

--- form/src/app/components/Form.tsx
+++ form/src/app/components/Form.tsx
@@ -8,7 +8,6 @@ import PictureComponent from "./PictureComponent"
 import ResumeComponent from "./ResumeComponent"
 import SchoolYearComponent from "./SchoolYearComponent";
 import GraduationYearComponent from "./GraduationYearComponent";
-import ClubsComponent from "./ClubsComponent";
 import WorkExperienceComponent from "./WorkExperienceComponent";
 import NameComponent from "./NameComponent";
 import BioComponent from "./BioComponent";
@@ -102,86 +101,9 @@ const Form = () => {
                 <EducationComponent value={formData.education} onChange={(next) => setFormData(prev => ({ ...prev, education: next }))} />
 
                 <div className="input work-experience">
-                    <h3>Work Experience</h3>
-                    {formData.workExperience.map((experience, index) => {
-                        return (
-                            <div
-                                key={`workExperience[${index}]`}
-                                className="experience"
-                            >
-                                <h4>Experience {index + 1}</h4>
-                                <input
-                                    type="text"
-                                    name={`workExperience[${index}].name`}
-                                    placeholder="Company Name"
-                                    value={experience.name}
-                                    onChange={(e) =>
-                                        setFormData((prev) => {
-                                            const result = [
-                                                ...prev.workExperience,
-                                            ];
-                                            result[index].name = e.target.value;
-                                            return {
-                                                ...prev,
-                                                workExperience: result,
-                                            };
-                                        })
-                                    }
-                                />
-                                <input
-                                    type="text"
-                                    name={`workExperience[${index}].title`}
-                                    placeholder="Job Title"
-                                    value={experience.title}
-                                    onChange={(e) =>
-                                        setFormData((prev) => {
-                                            const result = [
-                                                ...prev.workExperience,
-                                            ];
-                                            result[index].title =
-                                                e.target.value;
-                                            return {
-                                                ...prev,
-                                                workExperience: result,
-                                            };
-                                        })
-                                    }
-                                />
-                                <textarea
-                                    name={`workExperience[${index}].description`}
-                                    placeholder="Description"
-                                    value={experience.description}
-                                    onChange={(e) =>
-                                        setFormData((prev) => {
-                                            const result = [
-                                                ...prev.workExperience,
-                                            ];
-                                            result[index].description =
-                                                e.target.value;
-                                            return {
-                                                ...prev,
-                                                workExperience: result,
-                                            };
-                                        })
-                                    }
-                                    rows={3}
-                                />
-                            </div>
-                        );
-                    })}
-                    <input
-                        type="button"
-                        value="Add Experience"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            setFormData((prev) => ({
-                                ...prev,
-                                workExperience: [
-                                    ...prev.workExperience,
-                                    createEmptyExperience(),
-                                ],
-                            }));
-                        }}
+                    <WorkExperienceComponent
+                        workExperience = {formData.workExperience}
+                        setFormData = {setFormData}
                     />
                 </div>
 
