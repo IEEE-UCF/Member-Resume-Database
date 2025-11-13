12a2570016aa2ea8b98bad8ffb187657bf33cce0
Tal <tal.avital04@gmail.com>
2025-11-13 15:01:35 -0500

    fix work experience

--- website/client/src/app/components/Form.tsx
+++ website/client/src/app/components/Form.tsx
@@ -25,7 +25,7 @@ const dummyData = {
     resumeType: "link",
     major: "Computer Science",
     schoolYear: "Senior",
-    graduationYear: "2027",
+    graduationYear: 2027,
     links: [
         "https://www.linkedin.com/in/tal-avital-profile/",
         "https://github.com/TAvital04",
@@ -130,7 +130,9 @@ const Form = () => {
 
     return (
         <>
-            <div className="output name"></div>
+            <div className="output name">
+                <NameComponent name={formData.name} />
+            </div>
 
             <div className="output bio">
                 <BioComponent bio={formData.bio} />
@@ -140,11 +142,17 @@ const Form = () => {
                 <ResumeComponent resume={formData.resume} resumeType={formData.resumeType} />
             </div>
 
-            <div className="output major"></div>
+            <div className="output major">
+                <MajorComponent major={formData.major} />
+            </div>
 
-            <div className="output school-year"></div>
+            <div className="output school-year">
+                <SchoolYearComponent schoolYear={formData.schoolYear} />
+            </div>
 
-            <div className="output graduation-year"></div>
+            <div className="output graduation-year">
+                <GraduationYearComponent graduationYear={formData.graduationYear} />
+            </div>
 
             <div className="output links">
                 <LinksComponent links={formData.links} />
@@ -156,7 +164,9 @@ const Form = () => {
                 <EducationComponent education={formData.education} />
             </div>
 
-            <div className="output work-experience"></div>
+            <div className="output work-experience">
+                <WorkExperienceComponent workExperience={formData.workExperience} />
+            </div>
 
             <div className="output picture">
                 <PictureComponent picture={formData.picture} />
--- website/client/src/app/components/WorkExperienceComponent.tsx
+++ website/client/src/app/components/WorkExperienceComponent.tsx
@@ -1,39 +1,27 @@
+import { type Experience } from "../interfaces" 
+ 
+interface WorkExperienceComponentProps {
+    workExperience: Experience[];
+}
 
-interface Experience {
-    name: string;
-    description: string;
-    title: string;
-    skills: string[];
- }
- 
- 
- interface WorkExperienceComponentProps {
-    experiences: Experience[];
- }
- 
- 
- const WorkExperienceComponent = ({ experiences }: WorkExperienceComponentProps) => {
+
+const WorkExperienceComponent = ({ workExperience }: WorkExperienceComponentProps) => {
     return (
         <>
             {
-                experiences.map((exp, index) => (
+                workExperience.map((exp, index) => (
                     <div key={`${index}`}>
                         <h3>{exp.name}</h3>
                         <p><strong>Title:</strong> {exp.title}</p>
                         <p>{exp.description}</p>
-                         {
-                            exp.skills.map((skill, skillsIndex) => (
-                                <p key={`skill-${skillsIndex}`}>{skill}</p>
-                            ))
-                        }
                     </div>
                 ))
             }
         </>
     );
- };
- 
- 
- export default WorkExperienceComponent;
+};
+
+
+export default WorkExperienceComponent;
  
   
